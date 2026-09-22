da8ca287b4552ef6b05beecbad987dbafc416bd1
hiswon <ilovejudam@gmail.com>
2026-09-23 00:48:28 +0900

    11

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Firebase 콘솔에서 발급받은 실제 키 값들을 넣어주세요.
 const firebaseConfig = {
   apiKey: "AIzaSyBxtfEfVZ2FuUJraE6j4MJ2961aFLXkHl8",
   authDomain: "father-c31a0.firebaseapp.com",
@@ -13,9 +12,7 @@ const firebaseConfig = {
   measurementId: "G-EBQXEKXRHH"
 };
 
-// Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
-// 다른 파일에서 가져다 쓸 수 있도록 export
 export const auth = getAuth(app);
 export const db = getFirestore(app);
